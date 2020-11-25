09205d88e4da10b439e52fc5bc02c6c1530fbc69
Sam Bindoff <sdbindoff@gmail.com>
2020-11-25 12:19:01 +0000

    Styles method to accept paths array

--- src/helpers.php
+++ src/helpers.php
@@ -5,17 +5,10 @@ namespace SamBindoff;
 use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 
-function styles(array $rules = []): Config {
+function styles(array $paths = [], array $rules = []): Config {
     $rules = array_merge(require __DIR__.'/rules.php', $rules);
     $finder = Finder::create()
-        ->in([
-            __DIR__.'/app',
-            __DIR__.'/config',
-            __DIR__.'/database',
-            __DIR__.'/resources',
-            __DIR__.'/routes',
-            __DIR__.'/tests',
-        ]);
+        ->in($paths);
 
     return Config::create()
         ->setFinder($finder)
